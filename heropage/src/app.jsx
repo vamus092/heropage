7a2ca40b602950d906338f54086c2412bc32f9c8
vamus092 <dsuvam954@gmail.com>
2024-09-26 13:17:47 +0530

    app.jsx update

--- heropage/src/app.jsx
+++ heropage/src/app.jsx
@@ -1,10 +1,33 @@
+import React,{Suspense, useRef } from "react";
+import {Canvas} from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+import{Model} from "./model";
+import {Overlay} from "./overlay"
 import "./app.css"
+
+i//mport { Suspense } from "react";
 const App=()=>{
+
+    const overlay=useRef();
+    const caption=useRef();
+    const scroll=useRef(0);
+   
     return(
-    <div>
-        App
-    </div>
-    )
+        <>
+    <Canvas
+        shadows
+        eventSource={document.getElementById("root")}
+        eventPrefix={"client"}
+        >
+<ambientLight intensity={1}/>
+<Suspense fallback={null}>
+    <Model scroll={scroll}/>
+    <Environment preset="city"/>
+</Suspense>
+    </Canvas>
+    <Overlay caption={caption} scroll={scroll} ref={overlay}/>
+    </>
+    );
 }
 
 export default App;
